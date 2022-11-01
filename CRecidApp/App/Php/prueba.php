a457bf7c58e32a8bfd88f39df5852dde110dbf0a
estebj21 <94856862+estebj21@users.noreply.github.com>
2022-11-01 13:58:00 -0600

    Delete CRecidApp directory

--- CRecidApp/App/Php/prueba.php
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD:CRecidApp/CRecidApp/CRecidApp/App/Php/prueba.php
-<!DOCTYPE html>
-<html>
-=======
-<!DOCTYPE html>
-<html>
->>>>>>> e1e960f1adef3aac706fd99c2d00613c1a451887:CRecidApp/App/Php/prueba.php
-</HTML>
